4fe93eb8b6e45b31b0defdf3879b3abf723ab154
lee gyuseung <gyus0421@gmail.com>
2024-02-28 23:45:34 +0900

    Widget Lifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,13 +30,16 @@ class _AppState extends State<App> {
           ),
         ),
       ),
-      home: const Scaffold(
-        backgroundColor: Color(0xFFF4EDDB),
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              MyLargeTitle(),
+              showTitle ? const MyLargeTitle() : const Text('nothing'),
+              IconButton(
+                  onPressed: toggleTitle,
+                  icon: const Icon(Icons.remove_red_eye))
             ],
           ),
         ),
@@ -37,14 +48,35 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     super.key,
   });
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  @override
+  void initState() {
+    // 종종 부모 요소에 의존하는 데이터를 초기화 해야 하는 경우
+    // 항상 build 메서드보드 먼저 호출
+    super.initState();
+    print('initState!');
+  }
+
+  @override
+  void dispose() {
+    // 위젯이 스크린엥서 제거될 때 호출되는 메서드
+    super.dispose();
+    print('dispose!');
+  }
+
+  @override
   Widget build(BuildContext context) {
     // context는 MyLargeTitle Text의 부모 요소들의 모든 정보를 담음
+    print('build!');
     return Text(
       'My Large Title',
       style: TextStyle(
