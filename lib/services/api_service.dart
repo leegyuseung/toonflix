380bee53271c3795b3edda37c370d99ca694150a
lee gyuseung <gyus0421@gmail.com>
2024-03-06 23:24:21 +0900

    WEBTOON APP : fromJson

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,15 +1,23 @@
-import 'package:http/http.dart' as http; // namespace 설정
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart'; // namespace 설정
 
 class ApiService {
+  List<WebtoonModel> webtoonInstances = [];
   final String baseURL = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
     final url = Uri.parse('$baseURL/$today');
     final response = await http.get(url); // Future 타입 -> 비동기
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> webtoons =
+          jsonDecode(response.body); // dynamic 타입 -> 어떤 타입이든 수용
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
     }
     throw Error();
   }
