e4b1b627b8fd969123b4b5d3e8670f81b2c05afa
lee gyuseung <gyus0421@gmail.com>
2024-03-11 23:27:09 +0900

    WEBTOON APP : Hero

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -30,19 +30,22 @@ class DetailScreen extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                clipBehavior: Clip.hardEdge,
-                width: 250,
-                decoration: BoxDecoration(boxShadow: [
-                  BoxShadow(
-                    offset: const Offset(10, 10),
-                    color: Colors.black.withOpacity(0.3),
-                    blurRadius: 15,
-                  )
-                ], borderRadius: BorderRadius.circular(15)),
-                child: Image.network(
-                  thumb,
-                  headers: const {"Referer": 'https://comic.naver.com'},
+              Hero(
+                tag: id,
+                child: Container(
+                  clipBehavior: Clip.hardEdge,
+                  width: 250,
+                  decoration: BoxDecoration(boxShadow: [
+                    BoxShadow(
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.3),
+                      blurRadius: 15,
+                    )
+                  ], borderRadius: BorderRadius.circular(15)),
+                  child: Image.network(
+                    thumb,
+                    headers: const {"Referer": 'https://comic.naver.com'},
+                  ),
                 ),
               ),
             ],
